53975ded44aeca8c2a72c43d0c88a836c4f69612
AleksaU <32110183+AleksaU@users.noreply.github.com>
2018-02-20 16:08:12 +0300

    new function added

--- target/helloworld-1.0-SNAPSHOT/hello.js
+++ target/helloworld-1.0-SNAPSHOT/hello.js
@@ -11,65 +11,69 @@
  */
 
 function init() {
-	
-	// You need to pass the root path when you load your API
-	// otherwise calls to execute the API run into a problem
-	
-	// rootpath will evaulate to either of these, depending on where the app is running:
-	// //localhost:8080/_ah/api
-	// //your-app-id/_ah/api
 
-	var rootpath = "//" + window.location.host + "/_ah/api";
-	
-	// Load the helloworldendpoints API
-	// If loading completes successfully, call loadCallback function
-	gapi.client.load('helloworldendpoints', 'v1', loadCallback, rootpath);
+    // You need to pass the root path when you load your API
+    // otherwise calls to execute the API run into a problem
+
+    // rootpath will evaulate to either of these, depending on where the app is running:
+    // //localhost:8080/_ah/api
+    // //your-app-id/_ah/api
+
+    var rootpath = "//" + window.location.host + "/_ah/api";
+
+    // Load the helloworldendpoints API
+    // If loading completes successfully, call loadCallback function
+    gapi.client.load('helloworldendpoints', 'v1', loadCallback, rootpath);
 }
 
 /*
  * When helloworldendpoints API has loaded, this callback is called.
- * 
+ *
  * We need to wait until the helloworldendpoints API has loaded to
  * enable the actions for the buttons in index.html,
  * because the buttons call functions in the helloworldendpoints API
  */
-function loadCallback () {	
-	// Enable the button actions
-	enableButtons ();
+function loadCallback () {
+    // Enable the button actions
+    enableButtons ();
 }
 
 function enableButtons () {
-	// Set the onclick action for the first button
-	btn = document.getElementById("input_greet_generically");
-	btn.onclick= function(){greetGenerically();};
-	
-	// Update the button label now that the button is active
-	btn.value="Click me for a generic greeting";
-	
-	// Set the onclick action for the second button
-	btn = document.getElementById("input_greet_by_name");
-	btn.onclick=function(){greetByName();};
-	
-	// Update the button label now that the button is active
-	btn.value="Click me for a personal greeting";
+    // Set the onclick action for the first button
+    btn = document.getElementById("input_greet_generically");
+    btn.onclick= function(){greetGenerically();};
 
-    btn = document.getElementById("input_profession");
-    btn.onclick=function(){greetByProfession();};
+    // Update the button label now that the button is active
+    btn.value="Click me for a generic greeting";
+
+    // Set the onclick action for the second button
+    btn = document.getElementById("input_greet_by_name");
+    btn.onclick=function(){greetByName();};
 
     // Update the button label now that the button is active
     btn.value="Click me for a personal greeting";
+
+    btn = document.getElementById("input_profession");
+    btn.onclick=function(){greetByProf();};
+
+    // Update the button label now that the button is active
+    btn.value="Click me and I will know your profession";
+
+
+
+
 }
 
 /*
  * Execute a request to the sayHello() endpoints function
  */
 function greetGenerically () {
-	// Construct the request for the sayHello() function
-	var request = gapi.client.helloworldendpoints.sayHello();
-	
-	// Execute the request.
-	// On success, pass the response to sayHelloCallback()
-	request.execute(sayHelloCallback);
+    // Construct the request for the sayHello() function
+    var request = gapi.client.helloworldendpoints.sayHello();
+
+    // Execute the request.
+    // On success, pass the response to sayHelloCallback()
+    request.execute(sayHelloCallback);
 }
 
 /*
@@ -77,18 +81,8 @@ function greetGenerically () {
  * Illustrates calling an endpoints function that takes an argument.
  */
 function greetByName () {
-	// Get the name from the name_field element
-	var name = document.getElementById("name_field").value;
-	
-	// Call the sayHelloByName() function.
-	// It takes one argument "name"
-	// On success, pass the response to sayHelloCallback()
-	var request = gapi.client.helloworldendpoints.sayHelloByName({'name': name});
-	request.execute(sayHelloCallback);
-}
-function greetByProfession () {
     // Get the name from the name_field element
-    var name = document.getElementById("profession_field").value;
+    var name = document.getElementById("name_field").value;
 
     // Call the sayHelloByName() function.
     // It takes one argument "name"
@@ -97,11 +91,26 @@ function greetByProfession () {
     request.execute(sayHelloCallback);
 }
 
+function greetByProf () {
+    // Get the name from the name_field element
+    var prof = document.getElementById("profession_field").value;
+
+    // Call the sayHelloByName() function.
+    // It takes one argument "name"
+    // On success, pass the response to sayHelloCallback()
+    var request = gapi.client.helloworldendpoints.sayHelloByName({'name': prof});
+    request.execute(sayHelloCallback);
+}
+
+
+
+
+
 // Process the JSON response
 // In this case, just show an alert dialog box
 // displaying the value of the message field in the response
 function sayHelloCallback (response) {
-	alert(response.message);	
+    alert(response.message);
 }
 
 
